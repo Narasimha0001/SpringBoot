23f00cb5978526dade5afffde54db9b8e5e1fcbf
Mudhena Narasimharao <mudhenanarasimharao@gmail.com>
2025-08-08 15:23:47 +0530

    Added simple Spring Application

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -5,10 +5,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+		int a = 5;
 	@GetMapping("/")
 	public String Hello() {
-		return "Hello world, this is Rashi";
+		String message = "Hello world, this is Narasimha";
+ 
+        
+        int unusedVariable = 100;
+ 
+        
+        if (message.equals("Hello world, this is Narasimha")) {
+            System.out.println("Message is correct");
+        }
+ 
+        if (message.equals("Hello world, this is Narasimha")) {
+            System.out.println("Message is correct");
+        }
+ 
+
+		return "Hello world, this is Narasimha!";
 	}
+
+
+
+	
+
 
 }
